6ead818660447b96127d8a23fd857595864342a5
shinuamai <laurad00sandovalg@gmail.com>
2023-08-11 17:06:14 -0500

    new changes

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
-const Animal = mongoose.model('Animal', new mongoose.Schema({
+const Gatito = mongoose.model('Gatito', new mongoose.Schema({
   tipo: String,
   estado: String,
 }))
 
 const app = express()
 
-mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://nico:password@localhost:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
-  const animales = await Animal.find();
-  return res.send(animales)
+  console.log('listando... gatitos...')
+  const Gatitos = await Gatito.find();
+  return res.send(Gatitos)
 })
 app.get('/crear', async (_req, res) => {
   console.log('creando...')
-  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
+  await Gatito.create({ tipo: 'Gatito', estado: 'Feliz' })
   return res.send('ok')
 })
 
